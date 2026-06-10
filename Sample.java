f4378a721ca2a0d2aec34d66bc1a0abf0b1a4f25
Teja-630 <ramiganitejaswani@gmail.com>
2026-06-10 11:49:26 +0530

    Sample Arraylist Program Completed

---
+++ Sample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sample {
+    public static void main(String[] args){
+        ArrayList<String> a=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            String ar=sc.next();
+            a.add(ar);
+        }
+        System.out.print(a);
+    }
+}
