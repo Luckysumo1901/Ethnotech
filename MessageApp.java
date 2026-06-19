ec666ec216768326776adbb728a072306c2a82a5
Sumedha <sumedhavennapusa@gmail.com>
2026-06-19 09:23:21 +0530

    sql

--- MessageApp.java
+++ MessageApp.java
@@ -1,33 +1,38 @@
-
-
-class MessageSender extends Thread{
-    public MessageSender(String name){
-        super(name); //Setter
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+class MessageSender extends Thread {
+    public MessageSender(String name) {
+        super(name);
     }
-    @Override
-    public void run(){
+    public void run() {
         try{
-            for(int i=0;i<=3;i++){
-                System.out.println(getName()+"Sending Message");
+            for(int i=0;i<2;i++){
+                System.out.println("Message sent by "+Thread.currentThread().getName());
                 Thread.sleep(1000);
             }
-        }
-        catch(InterruptedException e){
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            System.out.println("Thread interrupted"+e);
+            System.out.println("Thread interrupted");   
         }
-        System.out.println("message send succesfully");
+        System.out.println("Message sent Successfully by "+Thread.currentThread().getName());
     }
 }
 public class MessageApp {
     public static void main(String[] args) {
-        MessageSender m1=new MessageSender("Sender1");
-        MessageSender m2=new MessageSender("Sender2");
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        for(int i=0;i<5;i++){
+            Runnable messageSender=new MessageSender(""+i);
+            executor.execute(messageSender);
+        }
+        executor.shutdown();
+        while(!executor.isTerminated()) {
+              }      System.out.println("All messages sent successfully");
+    
 
-        m1.start();
-        m2.start();
-        System.out.println("Message sent"+m1.isAlive()+m1.getState());
-        System.out.println("Message sent"+m2.isAlive()+m2.getState());
-
+        MessageSender t1 = new MessageSender("Thread 1");
+        MessageSender t2 = new MessageSender("Thread 2");
+        t1.start();
+        t2.start();
     }
+    
 }
